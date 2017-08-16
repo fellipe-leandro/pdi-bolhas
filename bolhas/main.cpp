9624e5f7bbeeb3adbfe07241d476514dc2c0b8b2
fellipe_leandro <faugusto12@gmail.com>
2017-08-16 14:53:14 -0300

    DevelopingV2

--- bolhas/main.cpp
+++ bolhas/main.cpp
@@ -8,7 +8,7 @@ int main()
     Mat image;
     int width,height;
     int nobj=0;
-    int pilha[50][2]={0};
+    int pilha[80][2]={0};
     int pilha_pos=0;
     int contPilhaPos=0;
     image = imread("bolhas.png",CV_LOAD_IMAGE_GRAYSCALE);
@@ -28,10 +28,11 @@ int main()
                 nobj++;
                 pilha[pilha_pos][0]=i;
                 pilha[pilha_pos][1]=j;
+                image.at<uchar>(pilha[pilha_pos][0],(pilha[pilha_pos][1]))=60;
                 while(pilha[0][0]!=-1){
 
                     if(image.at<uchar>(pilha[pilha_pos][0],pilha[pilha_pos][1]-1)==255){
-                        image.at<uchar>(pilha[pilha_pos][0],(pilha[pilha_pos][1]-1))=0;
+                        image.at<uchar>(pilha[pilha_pos][0],(pilha[pilha_pos][1]-1))=15;
                         //pilha_pos++;
                         contPilhaPos++;
                         pilha[contPilhaPos][0]=pilha[pilha_pos][0];
@@ -39,7 +40,7 @@ int main()
                     }
                     if(image.at<uchar>(pilha[pilha_pos][0],(pilha[pilha_pos][1]+1))==255){
                         //mudar cor
-                        image.at<uchar>(pilha[pilha_pos][0],(pilha[pilha_pos][1]+1))=0;
+                        image.at<uchar>(pilha[pilha_pos][0],(pilha[pilha_pos][1]+1))=15;
                         //pilha_pos++;
                         contPilhaPos++;
                         pilha[contPilhaPos][0]=pilha[pilha_pos][0];
@@ -48,7 +49,7 @@ int main()
                     if(image.at<uchar>(pilha[pilha_pos][0]+1,pilha[pilha_pos][1])==255){
 
                         //mudar cor
-                        image.at<uchar>(pilha[pilha_pos][0]+1,pilha[pilha_pos][1])=0;
+                        image.at<uchar>(pilha[pilha_pos][0]+1,pilha[pilha_pos][1])=15;
                         //pilha_pos++;
                         contPilhaPos++;
                         pilha[contPilhaPos][0]=pilha[pilha_pos][0]+1;
@@ -57,18 +58,21 @@ int main()
                     }
                     if(image.at<uchar>(pilha[pilha_pos][0]-1,pilha[pilha_pos][1])==255){
                         //mudar cor
-                        image.at<uchar>(pilha[pilha_pos][0]-1,pilha[pilha_pos][1])=0;
+                        image.at<uchar>(pilha[pilha_pos][0]-1,pilha[pilha_pos][1])=15;
                         //pilha_pos++
                         contPilhaPos++;
                         pilha[contPilhaPos][0] = pilha[pilha_pos][0]-1;
                         pilha[contPilhaPos][1] = pilha[pilha_pos][1];
                     }
-                    for(int aux=0;aux<contPilhaPos&&aux<49;aux++){
+                    for(int aux=0;aux<contPilhaPos&&aux<79;aux++){
+                        if(aux==70){
+                            cout<<"Perigo"<<endl;
+                        }
                         pilha[aux][0]=pilha[aux+1][0];
                         pilha[aux][1]=pilha[aux+1][1];
 
                     }
-                    for(int aux1=contPilhaPos;aux1<50;aux1++){
+                    for(int aux1=contPilhaPos;aux1<80;aux1++){
                             pilha[aux1][0]=-1;
                             pilha[aux1][1]=-1;
 
@@ -83,6 +87,7 @@ int main()
         }
     namedWindow("janela",WINDOW_AUTOSIZE);
     imshow("janela", image);
+    imwrite("fill.png", image);
     waitKey();
     cout<<"Numero de bolhas: "<<nobj<<endl;
         return 0;
